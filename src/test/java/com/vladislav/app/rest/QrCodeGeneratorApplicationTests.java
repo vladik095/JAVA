79b10b8c3fb06f682aeb5fa7a13a6980cdc6a634
vladislav <ilusevicv@gmail.com>
2024-02-18 00:02:32 +0300

    Описание ваших изменений

--- src/test/java/com/vladislav/app/rest/QrCodeGeneratorApplicationTests.java
+++ src/test/java/com/vladislav/app/rest/QrCodeGeneratorApplicationTests.java
@@ -3,6 +3,7 @@ package com.vladislav.app.rest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//vd
 @SpringBootTest
 class QrCodeGeneratorApplicationTests {
 
